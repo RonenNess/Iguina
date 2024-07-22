d10bb19f941ceedf9370c9127c684f51b8ec0b3b
Ronen <RonenNess@gmail.com>
2024-07-22 21:17:48 +0300

    added vertical line entity

---
+++ Iguina/Entities/VerticalLine.cs
@@ -0,0 +1,44 @@
+﻿using Iguina.Defs;
+
+
+namespace Iguina.Entities
+{
+    /// <summary>
+    /// A graphical vertical line that separates between entities.
+    /// </summary>
+    public class VerticalLine : Entity
+    {
+        /// <summary>
+        /// Create the vertical line.
+        /// </summary>
+        /// <param name="system">Parent UI system.</param>
+        /// <param name="stylesheet">Vertical line stylesheet.</param>
+        public VerticalLine(UISystem system, StyleSheet? stylesheet) : base(system, stylesheet)
+        {
+            IgnoreInteractions = true;
+        }
+
+        /// <summary>
+        /// Create the vertical line with default stylesheets.
+        /// </summary>
+        /// <param name="system">Parent UI system.</param>
+        public VerticalLine(UISystem system) : this(system, system.DefaultStylesheets.VerticalLines)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override Anchor GetDefaultEntityTypeAnchor()
+        {
+            return Anchor.AutoInlineLTR;
+        }
+
+        /// <inheritdoc/>
+        protected override MeasureVector GetDefaultEntityTypeSize()
+        {
+            var ret = new MeasureVector();
+            ret.X.SetPixels(8);
+            ret.Y.SetPercents(100f);
+            return ret;
+        }
+    }
+}
