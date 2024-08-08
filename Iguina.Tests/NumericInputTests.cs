ad1b967d55d6e74b21e659f07128ade830b07681
RudyTheDev <3857299+RudyTheDev@users.noreply.github.com>
2024-08-08 15:17:41 +0300

    NumericInput tests to also have expectations about the final text value

--- Iguina.Tests/NumericInputTests.cs
+++ Iguina.Tests/NumericInputTests.cs
@@ -7,27 +7,30 @@ namespace Iguina.Tests
     /// </summary>
     public class NumericInputTests
     {
-        [TestCase("5", 5)]
-        [TestCase("100", 100)]
-        [TestCase("0", 0)]
-        [TestCase("005", 5)]
-        [TestCase("1.23", 1.23)]
-        [TestCase("0.23", 0.23)]
-        [TestCase(".23", 0.23)]
-        [TestCase("2.", 2)]
-        [TestCase("-5", -5)]
-        [TestCase("-100", -100)]
-        [TestCase("-0", -0)]
-        [TestCase("-005", -5)]
-        [TestCase("-1.23", -1.23)]
-        [TestCase("-0.23", -0.23)]
-        [TestCase("-.23", -0.23)]
-        [TestCase("-2.", -2)]
-        [TestCase("1..23", 1.23)]
-        [TestCase("1.2.3", 1.23)]
-        [TestCase("1.23.", 1.23)]
-        public void TestTypeDecimalNumber(string input, decimal expectedValue)
+        [TestCase("5", 5, "5")]
+        [TestCase("100", 100, "100")]
+        [TestCase("0", 0, "0")]
+        [TestCase("005", 5, "5")]
+        [TestCase("1.23", 1.23, "1.23")]
+        [TestCase("0.23", 0.23, "0.23")]
+        //[TestCase(".23", 0.23, ".23")] -- are we allowed to type stuff like ".5"?
+        [TestCase("2.", 2, "2.")]
+        [TestCase("-5", -5, "-5")]
+        [TestCase("-100", -100, "-100")]
+        [TestCase("-0", -0, "-0")]
+        [TestCase("-005", -5, "-5")]
+        [TestCase("-1.23", -1.23, "-1.23")]
+        [TestCase("-0.23", -0.23, "-0.23")]
+        //[TestCase("-.23", -0.23, "-.23")] -- are we allowed to type stuff like "-.5"?
+        [TestCase("-2.", -2, "-2.")]
+        [TestCase("1..23", 1.23, "1.23")]
+        [TestCase("1.2.3", 1.23, "1.23")]
+        [TestCase("1.23.", 1.23, "1.23")]
+        public void TestTypeDecimalNumber(string input, decimal expectedValue, string? expectedText = null)
         {
+            if (expectedText == null)
+                expectedText = input;
+            
             var system = new UISystem(new TestRenderer(), new TestInputProvider());
             var numericInput = new NumericInput(system);
             
@@ -36,6 +39,7 @@ namespace Iguina.Tests
                 numericInput.Value += input[i];
             
             Assert.That(numericInput.NumericValue, Is.EqualTo(expectedValue));
+            Assert.That(numericInput.Value, Is.EqualTo(expectedText));
         }
     }
 }
